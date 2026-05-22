1095d207ae07d263071f74178ea45224d89827dc
MTHRafid <mthrafid2005@gmail.com>
2026-05-22 23:50:08 +0600

    Added new features and updated code

--- WindowsFormsApp1/MainWash.Designer.cs
+++ WindowsFormsApp1/MainWash.Designer.cs
@@ -32,8 +32,6 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.MLoginBtn = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
             this.richTextBox4 = new System.Windows.Forms.RichTextBox();
             this.richTextBox3 = new System.Windows.Forms.RichTextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -48,8 +46,6 @@
             this.panel3.BackColor = System.Drawing.Color.Aquamarine;
             this.panel3.Controls.Add(this.MLoginBtn);
             this.panel3.Controls.Add(this.richTextBox1);
-            this.panel3.Controls.Add(this.richTextBox5);
-            this.panel3.Controls.Add(this.richTextBox2);
             this.panel3.Controls.Add(this.richTextBox4);
             this.panel3.Controls.Add(this.richTextBox3);
             this.panel3.Location = new System.Drawing.Point(3, 0);
@@ -81,34 +77,12 @@
             this.richTextBox1.TabIndex = 24;
             this.richTextBox1.Text = "Mohammad Tahmedul Haque Rafid (ID: 23-53673-3)";
             // 
-            // richTextBox5
-            // 
-            this.richTextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox5.Enabled = false;
-            this.richTextBox5.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox5.Location = new System.Drawing.Point(223, 285);
-            this.richTextBox5.Name = "richTextBox5";
-            this.richTextBox5.Size = new System.Drawing.Size(96, 28);
-            this.richTextBox5.TabIndex = 28;
-            this.richTextBox5.Text = "Group: 10";
-            // 
-            // richTextBox2
-            // 
-            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBox2.Enabled = false;
-            this.richTextBox2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox2.Location = new System.Drawing.Point(77, 121);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(227, 55);
-            this.richTextBox2.TabIndex = 25;
-            this.richTextBox2.Text = "Jannatul Ferdous Reyan (ID: 21-44646-1)";
-            // 
             // richTextBox4
             // 
             this.richTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.richTextBox4.Enabled = false;
             this.richTextBox4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.richTextBox4.Location = new System.Drawing.Point(63, 285);
+            this.richTextBox4.Location = new System.Drawing.Point(123, 286);
             this.richTextBox4.Name = "richTextBox4";
             this.richTextBox4.Size = new System.Drawing.Size(96, 28);
             this.richTextBox4.TabIndex = 27;
@@ -172,8 +146,6 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button MLoginBtn;
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox5;
-        private System.Windows.Forms.RichTextBox richTextBox2;
         private System.Windows.Forms.RichTextBox richTextBox4;
         private System.Windows.Forms.RichTextBox richTextBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
